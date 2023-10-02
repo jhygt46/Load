81e0db7cd4f73b7f415a715cc82532282a5f4792
jhygt46 <misitiodelivery@gmail.com>
2023-10-02 19:41:30 -0300

    aa

--- LoadBalancer/prod.go
+++ LoadBalancer/prod.go
@@ -97,7 +97,7 @@ func (h *MyHandler) Send(data []byte) []byte {
 
 	num := h.Count % len(h.Servers)
 
-	fmt.Println(num)
+	fmt.Println(num, h.Count, len(h.Servers))
 	fmt.Println(h.Servers[num])
 
 	uri := fmt.Sprintf("http://%v:%v", h.Servers[num].addr, h.Servers[num].port)
